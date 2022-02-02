cca2d686b05e1cbec451ac0d1a45ac0e994af1a8
M SANJEEV <98035905+sanjureddysj@users.noreply.github.com>
2022-02-02 08:43:54 +0000

    P1 Original Exceuted

--- p1original.c
+++ p1original.c
@@ -1,2 +1,27 @@
+#include<stdio.h>
 
+void input(int*a, int*b)
+{
+  printf("Enter 2 numbers\n");
+  scanf("%d %d", a,b);
+}
+
+void add(int a, int b, int*sum)
+{
+  *sum=a+b;
+  return sum;
+}
+
+void output(int a, int b, int sum)
+{
+  printf("The Sum of Two numbers is %d", sum);
+}
+
+int main()
+{
+  int a,b,sum;
+  input(&a,&b);
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
 }
